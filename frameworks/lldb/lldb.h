7b0965af39a7fa82bdf3d9fc1d37ec76ce0229d2
Alfred Neumayer <dev.beidl@gmail.com>
2023-07-02 23:38:33 +0200

    frameworks: Begin work on shippable lldb

---
+++ frameworks/lldb/lldb.h
@@ -0,0 +1,19 @@
+//
+//  lldb.h
+//  lldb
+//
+//  Created by Alfred Neumayer on 27.06.23.
+//  Copyright © 2023 Nicolas Holzschuch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for lldb.
+FOUNDATION_EXPORT double lldbVersionNumber;
+
+//! Project version string for lldb.
+FOUNDATION_EXPORT const unsigned char lldbVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <lldb/PublicHeader.h>
+
+
